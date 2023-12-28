5ebd1cac8734f223c173d78c3b3827f99098551f
Nelson Lai <nelsonlai9503@gmail.com>
2023-12-28 19:25:00 +0800

    add nano generate functions

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Metadiv-Technology-Limited/nanoid
+
+go 1.21.5
+
+require github.com/matoous/go-nanoid v1.5.0
---
+++ internal/constant/constants.go
@@ -0,0 +1,11 @@
+package constant
+
+const (
+	NUMBERS    = "0123456789"
+	LOWERCASE  = "abcdefghijklmnopqrstuvwxyz"
+	UPPERCASE  = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	SYMBOLS    = "_-"
+	LOOK_ALIKE = "1lI0Oouv5Ss"
+
+	SAFE_LENGTH = 21
+)
